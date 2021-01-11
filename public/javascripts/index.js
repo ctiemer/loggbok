905f38f73dab9b07089f87cb7338f3d0bb59fc3b
ctiemer <carl.tiemer@gmail.com>
2021-01-11 18:45:32 +0100

    added basic functionality for saving to JSON

---
+++ public/javascripts/index.js
@@ -0,0 +1,15 @@
+
+
+/*document.getElementById('formadd').addEventListener('submit', e => { 
+    e.preventDefault();
+
+    /*var inputlog = document.getElementById('content').value;
+    var log = JSON.stringify([{ "content" : inputlog }]);
+    console.log(inputlog);
+    fs.writeFile('/data/logs.json', log , (err) => {
+        if (err) throw err;
+        console.log('saved');
+    }); 
+}); 
+
+*/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/', function(req, res, next) {
+  var log = {"content": req.body.content}
+  fs.writeFile(__dirname + '/../data/logs.json', JSON.stringify(log) , (err) => {
+    if (err) throw err;
+  });
+  res.redirect('/');
+});
 module.exports = router;
